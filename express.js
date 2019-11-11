4b7dd2ca3f817a0b178a1c336ed3a7d274f5ae46
Jerry Doran <Jerry.Doran@pccairfoils.com>
2019-11-11 14:01:55 -0500

    Added gitignore file

--- express.js
+++ express.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const Joi = require('joi');
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.use('/public', express.static(path.join(__dirname, 'static')));
 
 // Allows us to parse url encoded forms.
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Allows us to parse json data
+app.use(bodyParser.json());
 
 // SPIN UP THE SERVER
 const PORT = process.env.PORT || 3000;
@@ -24,16 +28,37 @@ app.get('/', (req, res) => {
 // POST ROUTE - TAKING IN INFORMATION FROM THE FORM ON THE CLIENT SIDE
 app.post('/', (req, res) => {
   console.log(req.body);
+  const schema = Joi.object().keys({
+    email: Joi.string()
+      .trim()
+      .email()
+      .required(),
+    password: Joi.string()
+      .min(5)
+      .max(10)
+      .required()
+  });
+
+  Joi.validate(req.body, schema, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.send('an error has occurred');
+    }
+    console.log(result);
+    res.send('successfully posted data');
+  });
   // database work here
 
-  res.send('successfully posted data');
+  // res.send('successfully posted data');
+
+  res.json({ success: true });
 });
 
 app.get('/example', (req, res) => {
   res.send('hitting example route');
 });
 
-//ROUTE PARAMETERS
+// ROUTE PARAMETERS
 app.get('/example/:name/:age', (req, res) => {
   console.log(req.params);
   console.log(req.query);
@@ -41,5 +66,3 @@ app.get('/example/:name/:age', (req, res) => {
     `Your name is ${req.params.name} and you are ${req.params.age} years old`
   );
 });
-
-// SERVING STATIC FILES
